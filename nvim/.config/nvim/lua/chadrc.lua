99d8c4bf7b2f20764023456609d25f4754fd2e47
Andrew Clarke <74689127+apscandy@users.noreply.github.com>
2024-06-30 19:52:04 +1000

    fix: nvim was missing init.lua

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,17 +1,8 @@
--- This file  needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
-
-M.general ={
-  n = {
-    ["<C-h>"] = {"<cmd> TmuxNavigateLeft<CR>","window left"},
-    ["<C-l>"] = {"<cmd> TmuxNavigateRight<CR>","window right"},
-    ["<C-j>"] = {"<cmd> TmuxNavigateDown<CR>","window down"},
-    ["<C-k>"] = {"<cmd> TmuxNavigateUp<CR>","window up"},
-  }
-}
 
 M.ui = {
 	theme = "catppuccin",
